0189176b2eaaf426514ee11843986817f4653bf9
Jordan Dains <dains.jordan2@gmail.com>
2020-09-26 14:32:04 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,7 +13,22 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  items = [
+    {:item => "AVOCADO", :price => 3.00, :clearance => true},
+    {:item => "KALE", :price => 3.00, :clearance => false},
+    {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+    {:item => "ALMONDS", :price => 9.00, :clearance => false},
+    {:item => "TEMPEH", :price => 3.00, :clearance => true},
+    {:item => "CHEESE", :price => 6.50, :clearance => false},
+    {:item => "BEER", :price => 13.00, :clearance => false},
+    {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+    {:item => "BEETS", :price => 2.50, :clearance => false},
+    {:item => "SOY MILK", :price => 4.50, :clearance => true}
+  ]
+  
   consolidate = []
+  items.each do |items|
+    if 
   count = 0
   
 end
